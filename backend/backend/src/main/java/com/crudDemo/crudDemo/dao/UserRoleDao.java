dda647209b2976d38b02add24218729fdf1b869d
Ali Bingöl <alii.bngll@gmail.com>
2024-12-26 20:18:31 +0300

    import

--- backend/backend/src/main/java/com/crudDemo/crudDemo/dao/UserRoleDao.java
+++
@@ -1,9 +0,0 @@
-package com.crudDemo.crudDemo.dao;
-
-import com.crudDemo.crudDemo.model.UserRole;
-
-public interface UserRoleDao extends AbstractDao{
-    UserRole findRolesByUserId(Long userId);
-
-    void deleteUserRole(Long userRoleId);
-}
